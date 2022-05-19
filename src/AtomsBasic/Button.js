b5f4ad927ea57d0d874449f4e8624c1f1795ecc6
christiian27173302 <chsotelogut@gmail.com>
2022-05-18 23:08:31 -0500

    update

---
+++ src/AtomsBasic/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Button() {
+
+    const name = 'button';
+    return ( 
+        <div>
+            <button>{name}</button>
+        </div>
+     );
+}
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Components/Header';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
